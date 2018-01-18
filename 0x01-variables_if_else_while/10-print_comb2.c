5ff242b54ddf9e6d4f8d706664ee8bb2eb59a452
Andrew Keil <monkeyninja147@gmail.com>
2018-01-18 22:34:55 +0000

    Initial Commit

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * purpose - putchar 00-99 with comma
+ *
+ * Return: (0) always
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 48 ; i < 58 ; i++)
+	{
+		for (j = 48 ; j < 58 ; j++)
+		{
+		putchar(i);
+		putchar(j);
+		if (i != 57 || j != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		}
+	}
+	putchar(10);
+	return (0);
+}

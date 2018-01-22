88e0f91a3e6d75ce1b6b56cf669c4c333265e8d5
Andrew Keil <monkeyninja147@gmail.com>
2018-01-22 19:14:29 +0000

    Initial Commit

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * purpose - putchar 00-99 with comma
+ *
+ * Return: (0) always
+ */
+
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar(10);
+	return (0);
+}

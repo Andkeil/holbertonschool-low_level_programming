89bc3b24ef5ba76b1b9c74aa74741eb3a96266c5
Andrew Keil <monkeyninja147@gmail.com>
2018-01-23 19:02:26 +0000

    Initial

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - check the code for Holberton School students.
+ *
+ * Return: void.
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+			_putchar(10);
+}

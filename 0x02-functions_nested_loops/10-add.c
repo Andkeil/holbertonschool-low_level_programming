d311c7bf9ac512953ef8ab6d10f3165c452cbca9
Andrew Keil <monkeyninja147@gmail.com>
2018-01-23 22:36:05 +0000

    Initial

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - check the code for Holberton School students.
+ * @a:numbers to add
+ * @b:numbers to add
+ * Return: integer.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}

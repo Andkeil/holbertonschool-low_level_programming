8e6ccb279f00957f25e54298070ce3c13b87c929
Andrew Keil <monkeyninja147@gmail.com>
2018-01-24 05:01:19 +0000

    initial

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * purpose - print natural numbers
+ *
+ * Return: (0) always
+ */
+
+int main(void)
+{
+	int i, sum;
+
+	sum = 0
+
+	for (i = 0; i <= 1024; i++)
+	{
+		if (i % 5 == 0 || i % 3 == 0)
+			sum += i;
+	}
+	return (sum);
+}

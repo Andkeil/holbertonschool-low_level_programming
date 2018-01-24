b55fc5bff5967bcf590f633183ee51b5c8eeec87
Andrew Keil <monkeyninja147@gmail.com>
2018-01-24 05:16:09 +0000

    initial

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * purpose - first 50 fib numbers
+ *
+ * Return: (0) always
+ */
+
+int main(void)
+{
+	long int i, j, k = 0, a = 1;
+
+	for (i = 0; i < 50; i++)
+	{
+		if (i <= 1)
+			j = i;
+		else
+		{
+			j = k + a;
+			k = a;
+			a = j;
+		}
+		printf("%li\n", j);
+	}
+	return (0);
+}

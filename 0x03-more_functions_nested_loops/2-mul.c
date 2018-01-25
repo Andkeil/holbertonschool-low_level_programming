c31617f1d4138ab676c7f680d93c1b3e56309775
Andrew Keil <monkeyninja147@gmail.com>
2018-01-25 23:18:01 +0000

    Initial

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - check the code for Holberton School students.
+ * @a:numbers to multiply
+ * @b:numbers to multiply
+ * Return: integer.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

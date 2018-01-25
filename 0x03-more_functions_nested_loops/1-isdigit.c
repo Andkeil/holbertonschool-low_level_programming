963907ceec1dc82e0a117f76817c17ff2566c6f1
Andrew Keil <monkeyninja147@gmail.com>
2018-01-25 23:12:13 +0000

    Initial

---
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _islower - check the code for Holberton School students.
+ * @i: number checked if it's digit
+ *
+ * Return: 1 or 0.
+ */
+
+int _isdigit(int c)
+{
+	if (c > 47 && c <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}

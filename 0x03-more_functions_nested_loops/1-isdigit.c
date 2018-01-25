3c9d1652e0774ab2d1130f9a2b2400778a71b41f
Andrew Keil <monkeyninja147@gmail.com>
2018-01-25 23:14:12 +0000

    Betty

--- 0x03-more_functions_nested_loops/1-isdigit.c
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _islower - check the code for Holberton School students.
+ * _isdigit - check the code for Holberton School students.
  * @i: number checked if it's digit
  *
  * Return: 1 or 0.
@@ -9,7 +9,7 @@
 
 int _isdigit(int c)
 {
-	if (c > 47 && c <= 57)
+	if (c >= 48 && c <= 57)
 	{
 		return (1);
 	}

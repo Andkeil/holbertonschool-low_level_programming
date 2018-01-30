5b4a2855fde482facf5b7c6c500063a841f919ca
Andrew Keil <monkeyninja147@gmail.com>
2018-01-30 23:56:41 +0000

    Initial

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * puts_half - Print second half of string and a newline
+ *
+ * @str: pointer to string
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i, length;
+
+	length = _strlen(str);
+
+	for (i = length / 2; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar(10);
+}
+
+/**
+ * _strlen - Return length of a string
+ *
+ * @s: pointer to string
+ *
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while  (s[i] != '\0')
+		i++;
+	return (i);
+}

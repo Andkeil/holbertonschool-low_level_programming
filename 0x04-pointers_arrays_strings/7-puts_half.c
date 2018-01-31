8aebc3d8c9156c842448b107a8c5c4c458a17819
Andrew Keil <monkeyninja147@gmail.com>
2018-01-31 00:00:49 +0000

    Handling for odd lengths

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -15,6 +15,10 @@ void puts_half(char *str)
 
 	length = _strlen(str);
 
+	if (length % 2 != 0)
+		for (i = (length - 1) / 2; i < length; i++)
+			_putchar(str[i]);
+	else
 	for (i = length / 2; i < length; i++)
 	{
 		_putchar(str[i]);

adf5b2963f81f92726c0e5ce3201c56dfa5a126c
Andrew Keil <monkeyninja147@gmail.com>
2018-02-06 02:05:34 +0000

    Initial

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strstr - find needle in haystack
+ *
+ * @haystack: string
+ * @needle: substring to find
+ *
+ * Return: char pointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, k;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0, k = i; needle[j] != '\0'; j++, k++)
+		{
+			if (haystack[k] != needle[j] || haystack[k] == '\0')
+			{
+				break;
+			}
+		}
+		if (needle[j] == '\0')
+		{
+			return (haystack + i);
+		}
+	}
+	return ('\0');
+}

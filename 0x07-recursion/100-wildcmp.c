74bc1737d47dc4393cb1cd82a4fe96c850b402ab
Andrew Keil <monkeyninja147@gmail.com>
2018-02-08 21:09:12 +0000

    Order fix

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -13,8 +13,6 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == *s2 && *s1 != '\0')
 		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*')
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	if (*s1 == '\0')
 	{
 		if (*s2 == '\0')
@@ -22,5 +20,7 @@ int wildcmp(char *s1, char *s2)
 		if (*s2 == '*')
 			return (wildcmp(s1, s2 + 1));
 	}
+	if (*s2 == '*')
+                return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	return (0);
 }

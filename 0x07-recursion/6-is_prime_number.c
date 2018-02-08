dcdc3a8235c3761ca27db03570bd619938a050af
Andrew Keil <monkeyninja147@gmail.com>
2018-02-08 04:42:56 +0000

    Initial

---
+++ 0x07-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - check if number is prime
+ *
+ * @n: number to check
+ *
+ * Return: 1 for prime 0 for other
+ */
+
+int is_prime_number(int n)
+{
+	if (n == -1 || n == 1)
+		return (0);
+	return (prime(n, 2));
+}
+
+/**
+ * prime - helper to find prime
+ *
+ * @n: number to check
+ * @i: incrementer
+ *
+ * Return: int
+ */
+
+int prime(int n, int i)
+{
+	if (i == n)
+		return (1);
+	if ((n % i) != 0)
+		return (prime(n, i + 1));
+	else
+		return (0);
+}

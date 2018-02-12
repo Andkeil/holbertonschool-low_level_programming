8eff19c4f217a3f7f93a9862199617a0496c3b40
Andrew Keil <monkeyninja147@gmail.com>
2018-02-12 01:10:42 +0000

    Initial

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#incldue <stdio.h>
+
+/**
+ * main - Prints name of program
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}

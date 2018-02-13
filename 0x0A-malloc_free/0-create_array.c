b59cec2ba9b043f18bc24b7139cb594ebea1ce25
Andrew Keil <monkeyninja147@gmail.com>
2018-02-13 23:17:26 +0000

    Initial

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creating an array using malloc
+ *
+ * @size: size the array
+ * @c: character to copy
+ *
+ * Return: char array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *p;
+
+	if (size == 0)
+		return (NULL);
+	p = malloc(size * sizeof(char));
+	for (i = 0; i < (signed int) size; i++)
+	{
+		*(p + i) = c;
+	}
+	return (p);
+}
---
+++ 0x0A-malloc_free/README.md
@@ -0,0 +1,6 @@
+Program 0: Create array of chars and initialize with char ||
+Program 1: Return a pointer to new space in memory with copy of string ||
+Program 2: Concat two strings ||
+Program 3: Return pointer to 2-D array of int ||
+Program 4: Free a 2-D grid from Program 3 ||
+Program 5: Concat all arguments of program

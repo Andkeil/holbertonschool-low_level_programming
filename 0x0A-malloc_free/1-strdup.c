40328d4b82af5932a73089393f4f34214c101480
Andrew Keil <monkeyninja147@gmail.com>
2018-02-14 00:28:24 +0000

    Initial

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * *_strdup - return pointer to new space with string
+ *
+ * @str: string
+ *
+ * Return: char array
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0, slen = 0;
+	char *dupstr;
+
+	if (str == NULL)
+		return (NULL);
+	while (*(str + i) != '\0')
+	{
+		slen ++;
+		i++;
+	}
+	i = 0;
+
+	dupstr = malloc((slen + 1) * sizeof(char));
+	while (i < slen + 1)
+	{
+		*(dupstr + i) = *(str + i);
+		i++;
+	}
+	*(dupstr + i) = '\0';
+	return (dupstr);
+}

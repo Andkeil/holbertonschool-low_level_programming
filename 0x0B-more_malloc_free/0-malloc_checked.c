71073ce227265f30b97a8c31ddad8b9872757989
Andrew Keil <monkeyninja147@gmail.com>
2018-02-15 19:20:10 +0000

    Initial

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - check if malloc failed
+ *
+ * @b: mem to allocate
+ *
+ * Return: pointer to mem allocated
+ */
+
+void *malloc_checked(unsigned int b);
+{
+	unsigned int *p;
+
+	p = malloc(b);
+	if (p == NULL || b == 0)
+	{
+		exit(98);
+	}
+	return (p);
+}
---
+++ 0x0B-more_malloc_free/README.md
@@ -0,0 +1,4 @@
+Function 0: Mallock with fail status of 98 ||
+Function 1: Concatenate two strings with limit of bytes for string 2 ||
+Function 2: Array memory malloc and calloc ||
+Function 3: Array of ints

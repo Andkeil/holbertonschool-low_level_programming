b3b7c055415e35fa3c34829adc38fd098107fdfe
Andrew Keil <monkeyninja147@gmail.com>
2018-02-15 22:46:28 +0000

    new pointer

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -14,15 +14,16 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i, *p;
+	unsigned int i;
+	char *p;
 
-	if (nmemb == 0 || size == 0 || nmemb >= UINT_MAX)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = malloc(nmemb * size * sizeof(int));
+	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb * size; i++)
-		p[i] = 0;
+		*(p + i) = 0;
 	return (p);
 }

56ed9b2772bd3dbc656152e2e0654514dbc2af2d
Andrew Keil <monkeyninja147@gmail.com>
2018-02-20 02:10:38 +0000

    Updates and Initials

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - initializing dog with given values
+ * @d: dog pointer
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}

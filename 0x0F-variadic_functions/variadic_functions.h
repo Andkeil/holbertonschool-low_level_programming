ed66e3676483a40a482daafb4d3ff5aa99f99074
Andrew Keil <monkeyninja147@gmail.com>
2018-02-24 19:29:10 +0000

    Fixed struct

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,5 +1,7 @@
 #ifndef VARIADIC_H
 #define VARIADIC_H
+#include <stdarg.h>
+#include <stdio.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
@@ -7,6 +9,10 @@ void print_all(const char * const format, ...);
 typedef struct type
 {
 	char *tp;
-	void (*f)()
+	void (*f)();
 }type_form;
+void print_c(va_list list);
+void print_i(va_list list);
+void print_fl(va_list list);
+void print_s(va_list list);
 #endif

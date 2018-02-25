071311ac486b2342e2354d284d53e4d5e28dde73
Andrew Keil <monkeyninja147@gmail.com>
2018-02-25 04:27:42 +0000

    betty

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,16 +1,25 @@
 #ifndef VARIADIC_H
 #define VARIADIC_H
+
 #include <stdarg.h>
 #include <stdio.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct type - data type
+ *
+ * @tp: type of char
+ * @f: The function associated
+ */
 typedef struct type
 {
 	char *tp;
 	void (*f)();
 } type_form;
+
 void print_c(va_list list);
 void print_i(va_list list);
 void print_fl(va_list list);

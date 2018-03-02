96a3f517db8af44141f2749cf82d05607d5c5952
Andrew Keil <monkeyninja147@gmail.com>
2018-03-02 03:39:52 +0000

    Cleaned up

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -12,18 +12,16 @@ size_t print_list(const list_t *h)
 {
 	int i = 0;
 
-	if (h == NULL)
-		return (0);
-	while (h)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-			i++;
 		}
+		h = h->next;
+		i++;
 	}
 	return (i);
 }

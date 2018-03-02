e464f1559b3abccb1daaeca1dc9f4779216d6219
Andrew Keil <monkeyninja147@gmail.com>
2018-03-02 03:26:19 +0000

    error conditioning

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -16,9 +16,14 @@ size_t print_list(const list_t *h)
 		return (0);
 	while (h)
 	{
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		i++;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+			i++;
+		}
 	}
 	return (i);
 }

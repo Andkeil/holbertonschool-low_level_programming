0843c85b743ce390b5a68764822563c3a3f80098
Andrew Keil <monkeyninja147@gmail.com>
2018-03-07 06:40:34 +0000

    Initial

---
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - pop head off of list
+ * @head: list
+ *
+ * Return: the integer popped off
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int num;
+
+	if (*head == NULL)
+		return (0);
+
+	tmp = (*head);
+	*head = NULL;
+	*head = tmp->next;
+	num = tmp->n;
+	free(temp);
+	return (num);
+}

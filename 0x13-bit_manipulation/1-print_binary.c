8d107e01d877d7089a6182d9632969f66b3b8c94
Andrew Keil <monkeyninja147@gmail.com>
2018-03-09 02:33:12 +0000

    Initial

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * print_binary - print binary rep of number
+ * @n: passed in number
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i = 0, copy = n, j = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+	else
+	{
+		while (copy > 0)
+		{
+			copy >>= 1;
+			j++;
+		}
+		for (i = j - 1; i >= 0; i--)
+		{
+			copy = n >> i;
+			if (copy & 1)
+				_putchar('1');
+			else
+				_putchar('0');
+		}
+	}
+}

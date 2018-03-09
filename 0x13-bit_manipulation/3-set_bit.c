432f4d3ba853e32724432bc9769a13533a9608f1
Andrew Keil <monkeyninja147@gmail.com>
2018-03-09 05:04:32 +0000

    Initial

---
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * set_bit - set bit value at given index
+ * @n: passed in number
+ * @index: index to set value at
+ *
+ * Return: value at index or -1 for error
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+		return (-1);
+	return (*n |= 1 << index);
+}

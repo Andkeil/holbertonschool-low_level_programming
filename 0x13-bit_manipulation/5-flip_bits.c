5e207953b1cba3d3fcd3948d85499790566ac188
Andrew Keil <monkeyninja147@gmail.com>
2018-03-09 05:41:11 +0000

    Initial

---
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * flip_bits - Find how many bits to flip from one # to another
+ * @n: passed in number
+ * @m: other passed in number
+ *
+ * Return: number of bits to flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int xoint, counter = 0;
+
+	xoint = (n ^ m);
+
+	while (xoint)
+	{
+		if (xoint & 1)
+			counter++;
+		xoint >>= 1;
+	}
+	return (counter);
+}

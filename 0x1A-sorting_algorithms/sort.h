dfb69014fd4927db693df0929ee9bbd88ce473ff
Andrew Keil <monkeyninja147@gmail.com>
2018-05-30 20:16:31 +0000

    attempted betty fix

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -37,4 +37,5 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void merging(int *a, int low, int mid, int high, int *b);
 void sort(int *a, int low, int high, int *b);
+
 #endif
